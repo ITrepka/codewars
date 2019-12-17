9556d621865d2b057e09dbc12cedc5286e62ec7c
Ireneusz Trepka <irek.trepka@gmail.com>
2019-12-17 01:41:58 +0100

    BlackOrWhiteKey

---
+++ src/main/java/exercises/BlackOrWhiteKey.java
@@ -0,0 +1,35 @@
+package exercises;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BlackOrWhiteKey {
+    public static String blackOrWhiteKey(int keyPressCount) {
+        // "white" - white, "black" - black
+        List<String> keys = new ArrayList<>();
+        keys.add("white");
+        keys.add("black");
+        keys.add("white");
+        keys.add("white");
+        for (int i = 0; i < 7; i++) {
+            keys.add("black");
+            keys.add("white");
+            keys.add("black");
+            keys.add("white");
+            keys.add("white");
+            keys.add("black");
+            keys.add("white");
+            keys.add("black");
+            keys.add("white");
+            keys.add("black");
+            keys.add("white");
+            keys.add("white");
+        }
+        int numberOfParts = keyPressCount / keys.size();
+        List<String> board = new ArrayList<>();
+        for (int i = 0; i < numberOfParts + 1; i++) {
+            board.addAll(keys);
+        }
+        return board.get(keyPressCount - 1);
+    }
+}
